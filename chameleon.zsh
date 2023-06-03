9dff17e84784eb17efe8a62a5d9197274d34762e
dr666m1 <skndr666m1@gmail.com>
2023-06-04 00:02:05 +0900

    wip color changes only once...

--- chameleon.zsh
+++ chameleon.zsh
@@ -1,9 +1,21 @@
+prompt_chameleon_change_color() {
+  local red16=${prompt_chameleon_color:0:2}
+  local green16=${prompt_chameleon_color:3:2}
+  local blue16=${prompt_chameleon_color:5:2}
+  local red10=$(echo "obase=10; ibase=16; $red16" | bc)
+  red10=$((red10 + 10))
+  red10=$((red10 < 255 ? red10 : 0))
+  red16=0$(echo "obase=16; ibase=10; $red10" | bc) # 1 -> 01, 00 -> 001
+  red16=${red16: -2} # 001 -> 01
+  prompt_chameleon_color='#'${red16}${green16}${blue16}
+}
+
 prompt_chameleon_refresh() {
-  prompt_chameleon_color='#ff0000'
-  zle reset-prompt
+  prompt_chameleon_change_color
+  echo $prompt_chameleon_color
 
   if $prompt_chameleon_async; then
-    async_job chameleon sleep 3
+    async_job chameleon sleep 1
   fi
 }
 
@@ -14,7 +26,7 @@ prompt_chameleon_precmd() {
 }
 
 prompt_chameleon_setup() {
-  typeset -g prompt_chameleon_color='#006688'
+  typeset -g prompt_chameleon_color='#000000'
   PROMPT='%F{$prompt_chameleon_color}> %f'
 
   typeset -g prompt_chameleon_async=false
@@ -26,10 +38,10 @@ prompt_chameleon_setup() {
     async_init
     async_start_worker chameleon
     async_register_callback chameleon prompt_chameleon_refresh
-    async_job chameleon sleep 3
+    async_job chameleon sleep 1
+    echo initialized
   else
-    echo async_init not found
-    add-zsh-hook precmd prompt_chameleon_precmd_fallback
+    add-zsh-hook precmd prompt_chameleon_precmd
   fi
 }
 
