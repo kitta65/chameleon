f040f1cde135bc69a983b258b8adf61c71949edd
dr666m1 <skndr666m1@gmail.com>
2023-06-03 22:06:48 +0900

    change prompt color once

--- chameleon.zsh
+++ chameleon.zsh
@@ -1,5 +1,11 @@
+prompt_chameleon_refresh() {
+  prompt_chameleon_color='#ff0000'
+  zle reset-prompt
+}
+
 prompt_chameleon_precmd() {
-  echo "dummy precmd"
+  TMOUT=1
+  trap "prompt_chameleon_refresh" SIGALRM
 }
 
 prompt_chameleon_setup() {
