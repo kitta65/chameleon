78582369681dcf3e5041a88396940156e77d71d2
dr666m1 <skndr666m1@gmail.com>
2023-06-03 17:14:11 +0900

    add dummy precmd

--- chameleon.zsh
+++ chameleon.zsh
@@ -1,6 +1,11 @@
-prompt_chameleon_setup () {
+prompt_chameleon_precmd() {
+  echo "dummy precmd"
+}
+
+prompt_chameleon_setup() {
   typeset -g prompt_chameleon_color='#006688'
   PROMPT='%F{$prompt_chameleon_color}> %f'
+  add-zsh-hook precmd prompt_chameleon_precmd
 }
 
 prompt_chameleon_setup "$@"
