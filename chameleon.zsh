456497eb8511f0fc356be144747cfdffa0ca21df
dr666m1 <skndr666m1@gmail.com>
2023-06-04 18:22:25 +0900

    refresh

--- chameleon.zsh
+++ chameleon.zsh
@@ -1,6 +1,22 @@
+prompt_chameleon_gradation() {
+  prompt_chameleon_color='#000000'
+}
+
+prompt_chameleon_refresh() {
+  prompt_chameleon_gradation
+  zle reset-prompt
+  async_job chameleon sleep $prompt_chameleon_interval
+}
+
 prompt_chameleon_setup() {
   typeset -g prompt_chameleon_color='#FF0000'
   PROMPT='%F{$prompt_chameleon_color}❯ %f'
   prompt_opts=(percent subst)
+
+  typeset -g prompt_chameleon_interval=0.5
+  async_init
+  async_start_worker chameleon
+  async_register_callback chameleon prompt_chameleon_refresh
+  async_job chameleon sleep $prompt_chameleon_interval
 }
 prompt_chameleon_setup "$@"
